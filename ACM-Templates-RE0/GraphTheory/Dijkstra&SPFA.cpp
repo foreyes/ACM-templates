f5f72e506aaf145d3da6c470aaa468f1d5c25d80
foreyes-surface <foreyes_1002@163.com>
2018-09-18 12:35:45 +0800

    fixed some bugs

--- ACM-Templates-RE0/GraphTheory/Dijkstra&SPFA.cpp
+++ ACM-Templates-RE0/GraphTheory/Dijkstra&SPFA.cpp
@@ -14,13 +14,13 @@ void dijkstra(int st,int n){
 	}
 }
 bool spfa(int st,int n){
-	ll cnt[maxn] = {0},sqrtn = sqrt(n) + 1;
+	ll cnt[maxn] = {0};
 	queue<pll> q;
 	for(int i = 1;i <= n;i++) dist[i] = 1e18+10;
 	dist[st] = 0;q.push(mp(0,st));
 	while(!q.empty()){
 		pll cur = q.front();q.pop();
-		if(++cnt[cur] > sqrtn) return false;
+		if(++cnt[cur.se] > n + 1) return false;
 		for(pll p : G[cur.se]){
 			if(dist[p.fi] > dist[cur.se] + p.se){
 				dist[p.fi] = dist[cur.se] + p.se;
