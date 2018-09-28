13235ead6a9aa96969bfdedaadd59dec8aed9825
foreyes-surface <foreyes_1002@163.com>
2018-09-28 08:59:30 +0800

    discretize template re-edited

--- ACM-Templates-RE0/UsefulTools/离散化.cpp
+++ ACM-Templates-RE0/UsefulTools/离散化.cpp
@@ -10,16 +10,17 @@ void discretize(int* f,int size){
 }
 
 //去重离散化，新的下标从1开始，最大下标为_cnt 
-int idx[maxn],tmp_id[maxn],_cnt;
-inline int get_id(int x){
-	return lower_bound(idx+1,idx+_cnt+1,x) - idx; 
+const int dis_maxn = 1e6+10;
+ll idx[dis_maxn], tmp_id[dis_maxn];
+int _cnt;
+int get_id(ll x){
+	return lower_bound(idx+1,idx+_cnt_1,x) - idx;
 }
-void discretize(int* f,int size){
-	_cnt = 0; 
-	for(int i = 1;i <= size;i++) tmp_id[i] = f[i];
-	sort(tmp_id+1,tmp_id+size+1);
+void discretize(ll f, int size){
+	_cnt = 0;
+	for(int i = 1;i <= n;i++) tmp_id[i] = f[i];
+	sort(tmp_id+1, tmp_id+size+1);
 	for(int i = 1;i <= size;i++){
-		idx[++_cnt] = tmp_id[i];
-		while(i + 1 <= size && tmp_id[i] == tmp_id[i+1]) i++;
+		if(i != 1 || tmp_id[i] != tmp_id[i-1]) idx[++_cnt] = tmp_id[i];
 	}
-} 
+}
