bd0d5c83dd608727e487932527983146e99c0544
foreyes-surface <foreyes_1002@163.com>
2018-10-02 11:40:26 +0800

    nothing

--- ACM-Templates-RE0/ComputationalGeometry/ComputationalGeometry.cpp
+++ ACM-Templates-RE0/ComputationalGeometry/ComputationalGeometry.cpp
@@ -52,11 +52,11 @@ Point rotate(Point a,double rad){
 //或者把线段端点往里缩一下，同时取中点，check一下这三个点是不是在多边形内部 
 bool isSegmentsIntersection(Point A,Point B,Point C,Point D){
 	//跨立试验 
-	if(Cross(C-A,D-A) * Cross(C-B,D-B) >= 0) return false;
-	if(Cross(A-C,B-C) * Cross(A-D,B-D) >= 0) return false;
+	if(dcmp(Cross(C-A,D-A) * Cross(C-B,D-B)) >= 0) return false;
+	if(dcmp(Cross(A-C,B-C) * Cross(A-D,B-D)) >= 0) return false;
 	//快速排斥试验 
-	if(min(max(A.x,B.x),max(C.x,D.x)) < max(min(A.x,B.x),min(C.x,D.x))) return false;
-	if(min(max(A.y,B.y),max(C.y,D.y)) < max(min(A.y,B.y),min(C.y,D.y))) return false;
+	if(dcmp(min(max(A.x,B.x),max(C.x,D.x)) - max(min(A.x,B.x),min(C.x,D.x))) < 0) return false;
+	if(dcmp(min(max(A.y,B.y),max(C.y,D.y)) - max(min(A.y,B.y),min(C.y,D.y))) < 0) return false;
 	return true;
 }
 //点在线段上(//不含端点)
@@ -106,12 +106,13 @@ double distanceToLine(Point P,Line L){
 }
 //----------------多边形相关内容----------------------
 typedef vector<Point> polygon;
+//调用时最好zoom(poly, eps*1000) 或者 zoom(poly, sqrt(eps))
 void zoom(polygon& poly, double rate){
 	int n = poly.size();
 	vector<Point> tmp;
 	for(int i = 0;i < n;i++) tmp.push_back(unitNormal(poly[i] - poly[(i-1+n)%n]));
 	for(int i = 0;i < n;i++){
-		poly[i] = poly[i] + (unit(tmp[i] + tmp[(i+1)%n]) * rate);
+		poly[i] = poly[i] + ((tmp[i] + tmp[(i+1)%n]) * rate);
 	}
 }
 //-----------------圆相关内容-----------------------
