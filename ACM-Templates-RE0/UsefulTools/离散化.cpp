f961e1789a8c67afeab5dcf0584c903538627b41
foreyes-surface <foreyes_1002@163.com>
2018-10-10 19:10:07 +0800

    fixed discretize

--- ACM-Templates-RE0/UsefulTools/离散化.cpp
+++ ACM-Templates-RE0/UsefulTools/离散化.cpp
@@ -1,9 +1,26 @@
-int main(){
-	cin>>n;
-	for(int i = 0;i < n;i++) cin>>a[i];
-	memset(dp,0x3f,sizeof(dp));
-	for(int i = 0;i < n;i++){
-		*lower_bound(dp,dp+n,a[i]) = a[i];
+//无去重离散化，新的下标从1开始，最大下标为_cnt 
+ int idx[maxn],_cnt;
+ inline int get_id(int x){
+ 	return lower_bound(idx+1,idx+_cnt+1,x) - idx; 
+ }
+ void discretize(int* f,int size){
+ 	for(int i = 1;i <= size;i++) idx[i] = f[i];
+ 	sort(idx+1,idx+size+1);
+ 	_cnt = size;
+ }
+
+ //去重离散化，新的下标从1开始，最大下标为_cnt 
+ const int dis_maxn = 1e6+10;
+ ll idx[dis_maxn], tmp_id[dis_maxn];
+ int _cnt;
+ int get_id(ll x){
+ 	return lower_bound(idx+1,idx+_cnt_1,x) - idx;
+ }
+ void discretize(ll f, int size){
+ 	_cnt = 0;
+ 	for(int i = 1;i <= n;i++) tmp_id[i] = f[i];
+ 	sort(tmp_id+1, tmp_id+size+1);
+ 	for(int i = 1;i <= size;i++){
+ 		if(i != 1 || tmp_id[i] != tmp_id[i-1]) idx[++_cnt] = tmp_id[i];
 	}
-	cout<<lower_bound(dp,dp+n,0x3f3f3f3f) - dp - 1;
-}
+} 
