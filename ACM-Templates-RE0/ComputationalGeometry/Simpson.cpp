6356532322430f6db63e0f07f1dccd013ae29d8c
foreyes-surface <foreyes_1002@163.com>
2018-10-12 08:50:34 +0800

    simpson added

---
+++ ACM-Templates-RE0/ComputationalGeometry/Simpson.cpp
@@ -0,0 +1,19 @@
+double F(double x){
+	//Simpson公式用到的函数
+}
+//三点Simpson法，这里要求F是一个全局函数
+double simpson(double a, double b) {
+	double c = a + (b - a) / 2;
+	return (F(a) + 4 * F(c) + F(b))*(b - a) / 6;
+}
+//自适应Simpson公式（递归过程）。已知整个区间[a,b]上的三点Simpson值A
+double asr(double a, double b, double eps, double A){
+	double c = a + (b - a) / 2;
+	double L = simpson(a, c), R = simpson(c, b);
+	if (fabs(L + R - A) <= 15 * eps)return L + R + (L + R - A) / 15.0;
+	return asr(a, c, eps / 2, L) + asr(c, b, eps / 2, R);
+}
+//自适应Simpson公式（主过程）
+double asr(double a, double b, double eps) {
+	return asr(a, b, eps, simpson(a, b));
+}
