11f04e91bfb6b75fd746faaa55b7e13a7ee1e755
foreyes-surface <foreyes_1002@163.com>
2018-11-09 11:40:38 +0800

    latest template update

--- ACM-Templates-RE0/ComputationalGeometry/ComputationalGeometry.cpp
+++ ACM-Templates-RE0/ComputationalGeometry/ComputationalGeometry.cpp
@@ -164,3 +164,18 @@ Circle getMinCircle(Point a,Point b,Point c){
 bool pointInCircle(Point a,Circle c){
 	return dcmp(Length2(a-c.o)-c.r*c.r) <= 0;
 }
+//两圆的面积交
+double Ac(Point c1, double r1, Point c2, double r2)
+{
+    double d = dist(c1,c2);
+    if(r1 + r2 < d + eps) return 0;
+    if(d < fabs(r1 - r2) + eps)
+    {
+        double r = min(r1,r2);
+        return PI*r*r;
+    }
+    double x = (d*d + r1*r1 - r2*r2)/(2*d);
+    double t1 = acos(x / r1);
+    double t2 = acos((d - x)/r2);
+    return r1*r1*t1 + r2*r2*t2 - d*r1*sin(t1);
+}
